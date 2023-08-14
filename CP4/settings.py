f55706fddf2b65cdac8125b8f707b8f0fa9d80ec
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-08-14 15:51:29 -0400

    construindo

--- CP4/settings.py
+++ CP4/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig',
+    'core',
+    'bootstrap4',
+    'stdimage',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import IndexView
 
 urlpatterns = [
-    path('inicio/', IndexView.as_view(), name='inicio'),
+    path('', IndexView.as_view(), name='inicio'),
 ]
