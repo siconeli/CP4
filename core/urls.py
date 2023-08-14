f58fc410a8b5de7c768e8dc46a276e4261c68c9f
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-08-14 16:08:34 -0400

    construindo

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, BaseView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='inicio'),
+    path('base/', BaseView.as_view(), name='base'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,5 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'core/index.html'
     
+class BaseView(TemplateView):
+    template_name = 'core/base.html'
