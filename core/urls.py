c8bd773debefc549a6ee8752f790086b65bdee49
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-08-15 10:20:08 -0400

    construindo

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import IndexView, CadProcessoAdmView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='inicio'),
+    path('', IndexView.as_view(), name='index'),
     path('administrativo/', CadProcessoAdmView.as_view(), name='cadprocadm'),
 ]
