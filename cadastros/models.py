c84cd373b0e01966a4bd2ee09929fd5c0e37a2f0
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-08-17 14:39:40 -0400

    construindo

--- cadastros/models.py
+++ cadastros/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Base(models.Model): #Classe para registrar no banco de dados, data de criação e modificação dos registros.
+class Base(models.Model): # Classe para registrar no banco de dados, data de criação e modificação dos registros.
     criados = models.DateField('Criação', auto_now_add=True)
     modificado = models.DateField('Atualização', auto_now=True)
     ativo = models.BooleanField('Ativo?', default=True)
@@ -10,28 +10,28 @@ class Base(models.Model): #Classe para registrar no banco de dados, data de cria
         abstract = True
 
     
-class UnidadeFederativa(Base):  #Não terá um formulário para cadastro, irei cadastrar todas as uf através do painel admin.
+class UnidadeFederativa(Base):  # Não terá um formulário para cadastro, irei cadastrar todas as uf através do painel admin.
     uf = models.CharField(max_length=50) 
 
     def __str__(self):
         return f'{self.uf}'
 
 
-class ProcessoAdministrativo(Base): #Cadastro de processo administrativo
-    pat = models.IntegerField(verbose_name='PAT N°', unique=True) #PAT n° - Número do processo
-    munic = models.CharField(max_length=50, verbose_name='Município') #Município
+class ProcessoAdministrativo(Base): # Cadastro de processo administrativo
+    pat = models.IntegerField(verbose_name='PAT N°', unique=True) # PAT n° - Número do processo
+    munic = models.CharField(max_length=50, verbose_name='Município') # Município
     uf = models.ForeignKey(UnidadeFederativa, on_delete=models.PROTECT) # O 'PROTECT' protege na hora de um delete, não deixa excluir se possuir algum vínculo.
-    datini = models.DateField(verbose_name='Data Inicial') #Data inicial
-    datfin = models.DateField(verbose_name='Data Final') #Data final
-    datdivat = models.DateField(verbose_name='Data Dívida Ativa') #Data dívida ativa
-    valtrib = models.DecimalField(decimal_places=2 , max_digits=9, verbose_name='Valor do Atributo') #Valor do Tributo
-    valmul = models.DecimalField(decimal_places=2 , max_digits=9, verbose_name='Valor da Multa') #Valor da Multa
-    valcred = models.DecimalField(decimal_places=2 , max_digits=9, verbose_name='Valor do Crédito') #Valor do Crédito
-    valatu = models.DecimalField(decimal_places=2 , max_digits=9, verbose_name='Valor do Atualizado') #Valor do Atualizado
-    datvalatu = models.DateField(verbose_name='Data Valor Atualizado') #Data valor atualizado
-    datand = models.DateField(verbose_name='Data Andamento') #Data andamento
-    anda = models.IntegerField(verbose_name='Andamento Atual') #Andamento atual
-    datprazo = models.DateField(verbose_name='Data Prazo') #Data prazo
+    datini = models.DateField(verbose_name='Data Inicial') # Data inicial
+    datfin = models.DateField(verbose_name='Data Final') # Data final
+    datdivat = models.DateField(verbose_name='Data Dívida Ativa') # Data dívida ativa
+    valtrib = models.DecimalField(decimal_places=2 , max_digits=9, verbose_name='Valor do Atributo') # Valor do Tributo
+    valmul = models.DecimalField(decimal_places=2 , max_digits=9, verbose_name='Valor da Multa') # Valor da Multa
+    valcred = models.DecimalField(decimal_places=2 , max_digits=9, verbose_name='Valor do Crédito') # Valor do Crédito
+    valatu = models.DecimalField(decimal_places=2 , max_digits=9, verbose_name='Valor do Atualizado') # Valor do Atualizado
+    datvalatu = models.DateField(verbose_name='Data Valor Atualizado') # Data valor atualizado
+    datand = models.DateField(verbose_name='Data Andamento') # Data andamento
+    anda = models.IntegerField(verbose_name='Andamento Atual') # Andamento atual
+    datprazo = models.DateField(verbose_name='Data Prazo') # Data prazo
     upload = models.FileField(upload_to='uploads/', verbose_name='Arquivo Processo')  # Para fazer upload de arquivos
 
     def __str__(self):
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,17 +1,20 @@
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView # Módulo para criar, atualizar e deletar
+from django.views.generic.list import ListView # Módulo para listar
 
 from .models import ProcessoAdministrativo
 
 from django.urls import reverse_lazy
 
+from django.contrib.auth.mixins import LoginRequiredMixin # Módulo para controlar o acesso de um usuário a determinada url, através de autenticação de login
+
 
 ###### CREATE ######
-class CadProcessoAdmCreate(CreateView):
+class CadProcessoAdmCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     model = ProcessoAdministrativo
     fields = ['pat', 'munic', 'uf', 'datini', 'datfin', 'datdivat', 'valtrib', 'valmul', 'valcred', 'valatu', 'datvalatu', 'datand', 'anda', 'datprazo', 'upload']
     template_name = 'cadastros/cadprocessoadm-cadastrar.html'
-    success_url = reverse_lazy('list-proc-adm')  #name da url, irá direcionar para a url
+    success_url = reverse_lazy('list-proc-adm')  # name da url, irá direcionar para a url 
 
 
 ###### UPDATE ######
