06968fd36b37795cae3a1a70527a8635f1e551fd
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-09-05 15:36:27 -0400

    Construindo

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from .views import IndexView
 
 
 urlpatterns = [
-    path('inicio/', IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='index'),
 ]
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views   # Views naturais do django para autenticação de usuário, não preciso criar uma view manuamente.
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(
+    path('login/', auth_views.LoginView.as_view(
         template_name = 'usuarios/login.html'  # Como eu não criei a view, utilizei uma já existente do módulo e informo o template aqui.
     ), name="login"),
 
